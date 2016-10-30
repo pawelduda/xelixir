48c3cbfec39c96c371a044fdbeb4ba739551bec0
pawelduda <pawelduda91@gmail.com>
2016-10-30 02:16:56 +0200

    Solve beer-song

--- exercises/beer-song/beer_song.exs
+++ exercises/beer-song/beer_song.exs
@@ -3,15 +3,39 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t
+  def verse(number) when number == 1 do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(number) when number == 2 do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(number) when number == 3 do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
   def verse(number) do
-    # Your implementation here...
+    """
+    #{number - 1} bottles of beer on the wall, #{number - 1} bottles of beer.
+    Take one down and pass it around, #{number - 2} bottles of beer on the wall.
+    """
   end
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
   """
   @spec lyrics(Range.t) :: String.t
-  def lyrics(range) do
-    # Your implementation here...
+  def lyrics(range \\ 100..1) do
+    Enum.map(range, &verse(&1)) |> Enum.join("\n")
   end
 end
